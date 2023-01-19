c26b21acd69290eee37357e6d4e1c47ab1357f2a
code1379 <coderxialuo@gmail.com>
2023-01-20 00:24:53 +0800

    feat: 删除无用代码，header组件的实现

---
+++ front/src/components/header/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="m-header">
+    <img class="logo" alt="">
+    <h4 class="title">chicken music</h4>
+  </div>
+</template>
+
+<script setup>
+import { defineComponent } from "vue";
+
+defineComponent({
+  name: "m-header"
+})
+</script>
+
+<style lang="scss" scoped>
+.m-header {
+  height: 44px;
+  text-align: center;
+  color: $color-theme;
+  font-size: 0;
+  .logo {
+    display: inline-block;
+    vertical-align: top;
+    width: 30px;
+    height: 32px;
+    @include bg-image("./logo");
+    background-size: 30px 32px;
+    margin-right: 6px;
+    margin-top: 6px;
+  }
+  .title {
+    display: inline-block;
+    vertical-align: top;
+    line-height: 44px;
+    font-size: $font-size-large;
+  }
+}
+</style>
